1f759d19de2e07220ff801c99156f2acd6a344c3
Denilson Telaroli <ddts80@gmail.com>
2020-05-09 15:33:47 -0300

    Log request

--- api/src/graphql/context.js
+++ api/src/graphql/context.js
@@ -1,8 +1,8 @@
 const { config } = require("../lib");
 
 const context = params => {
+  console.log(params);
   const { body } = params.req;
-  console.log(body);
   if (body.TopicArn === config.NOTIFICATION_ARN) {
     params.res.redirect(body.SubscribeURL);
     return params;
