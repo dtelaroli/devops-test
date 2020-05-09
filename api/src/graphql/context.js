46181a989afd19d12a37bc6dfadace11e1e2fc3b
Denilson Telaroli <ddts80@gmail.com>
2020-05-09 15:55:25 -0300

    Json

--- api/src/graphql/context.js
+++ api/src/graphql/context.js
@@ -1,9 +1,9 @@
 const { config } = require("../lib");
 
 const context = params => {
-  console.log(params);
-  const { body } = params.req;
-  if (body.TopicArn === config.NOTIFICATION_ARN) {
+  console.log(JSON.stringify(params, null, 2));
+  const { headers, body } = params.req;
+  if (headers["x-amz-sns-topic-arn"] === config.NOTIFICATION_ARN) {
     params.res.redirect(body.SubscribeURL);
     return params;
   }
