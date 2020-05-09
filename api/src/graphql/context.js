9207eaee37d998dd941fa0a5b2089281a1546d7d
Denilson Telaroli <ddts80@gmail.com>
2020-05-09 16:05:31 -0300

    Circurlar json

--- api/src/graphql/context.js
+++ api/src/graphql/context.js
@@ -1,7 +1,7 @@
 const { config } = require("../lib");
 
 const context = params => {
-  console.log(JSON.stringify(params, null, 2));
+  console.log(JSON.stringify(params.req, null, 2));
   const { headers, body } = params.req;
   if (headers["x-amz-sns-topic-arn"] === config.NOTIFICATION_ARN) {
     params.res.redirect(body.SubscribeURL);
