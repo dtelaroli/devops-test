9dcf82ed495172382f9e6d8f15a48071c853fbad
Denilson Telaroli <ddts80@gmail.com>
2020-05-10 04:22:07 -0300

    Remove custom credential

--- api/src/lib/aws.js
+++ api/src/lib/aws.js
@@ -1,8 +1,6 @@
 const AWS = require("aws-sdk");
 const config = require("./config");
 
-const credentials = new AWS.Credentials(config.AWS_ACCESS_KEY_ID, config.AWS_SECRET_ACCESS_KEY, config.AWS_SESSION_TOKEN);
-
-AWS.config.update({ region: config.REGION, credentials });
+AWS.config.update({ region: config.REGION });
 
 module.exports = AWS;
--- api/src/lib/config.js
+++ api/src/lib/config.js
@@ -2,6 +2,4 @@ const config = {
   ...process.env
 };
 
-console.log(config);
-
 module.exports = config;
