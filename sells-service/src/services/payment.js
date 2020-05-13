4895e1f8fe691d6f139a06443828b4c86dd84570
Denilson Telaroli <ddts80@gmail.com>
2020-05-13 20:04:35 -0300

    Add permission to lambda

--- sells-service/src/services/payment.js
+++ sells-service/src/services/payment.js
@@ -1,20 +1,20 @@
 const { stepFunctionsSuccess, config, post, throwIfError, stringify } = require("../lib");
 const { GATEWAY_URL } = config;
 
-const confirmation = async (body) => {
-  const input = {
-    output: stringify(body),
-    taskToken: body.taskToken,
-  };
-  return stepFunctionsSuccess(input);
-};
-
 const pay = async (body) => {
   const [error, result] = await post(GATEWAY_URL, body);
 
   throwIfError(error);
 
   return result.data;
+};
+
+const confirmation = async (body) => {
+  const input = {
+    output: stringify(body),
+    taskToken: body.taskToken,
+  };
+  return stepFunctionsSuccess(input);
 };
 
 module.exports = {
--- sells-service/src/services/shipment.js
+++ sells-service/src/services/shipment.js
@@ -1,20 +1,20 @@
 const { stepFunctionsSuccess, config, post, throwIfError, stringify } = require("../lib");
 const { COURIER_URL } = config;
 
-const confirmation = async (body) => {
-  const input = {
-    output: stringify(body),
-    taskToken: body.taskToken,
-  };
-  return stepFunctionsSuccess(input);
-};
-
 const send = async (body) => {
   const [error, result] = await post(COURIER_URL, body);
 
   throwIfError(error);
 
   return result.data;
+};
+
+const confirmation = async (body) => {
+  const input = {
+    output: stringify(body),
+    taskToken: body.taskToken,
+  };
+  return stepFunctionsSuccess(input);
 };
 
 module.exports = {
