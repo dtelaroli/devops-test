8b2e8557e00f5e1c0f5e3db76a047bb19d5712ce
Denilson Telaroli <ddts80@gmail.com>
2020-05-13 01:57:25 -0300

    Ste continue name

--- sells-service/src/controllers/sell.js
+++ sells-service/src/controllers/sell.js
@@ -1,16 +1,10 @@
-const { config, stepFunctionsStart, stringify, parse } = require("../lib");
+const { stepFunctionsStart, parse } = require("../lib");
 
 const sell = async (event) => {
   const results = Promise.all(
     event.Records.map((record) => {
       const body = parse(record.body);
-      const params = {
-        name: body.id,
-        stateMachineArn: config.STATE_MACHINE_ARN,
-        input: stringify(body),
-      };
-
-      return stepFunctionsStart(params);
+      return stepFunctionsStart(body);
     })
   );
 
