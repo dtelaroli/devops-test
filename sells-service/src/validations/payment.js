42c253457ff2932575000bf48ad8fba3076a4b7c
Denilson Telaroli <ddts80@gmail.com>
2020-05-14 19:00:28 -0300

    Change resultpath

--- sells-service/src/validations/payment.js
+++ sells-service/src/validations/payment.js
@@ -8,7 +8,7 @@ const payment = {
       status: Joi.string().required(),
     }),
     taskToken: Joi.string().required(),
-    url: Joi.string().required(),
+    url: Joi.string(),
   }),
 };
 
--- sells-service/src/validations/shipment.js
+++ sells-service/src/validations/shipment.js
@@ -8,7 +8,7 @@ const shipment = {
       status: Joi.string().required(),
     }),
     taskToken: Joi.string().required(),
-    url: Joi.string().required(),
+    url: Joi.string(),
   }),
 };
 
