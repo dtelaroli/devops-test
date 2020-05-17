83f1dc236505f792a2f4390893be6c50853b0e91
Denilson Telaroli <ddts80@gmail.com>
2020-05-17 18:35:39 -0300

    Add monitoring and more docs

--- iaas/applications/monitoring/locals.tf
+++ iaas/applications/monitoring/locals.tf
@@ -1,5 +1,29 @@
 locals {
-  name = "monitoring"
-  env                   = data.terraform_remote_state.base.outputs.env
-  region                = data.terraform_remote_state.base.outputs.region
+  name                = "monitoring"
+  env                 = data.terraform_remote_state.base.outputs.env
+  region              = data.terraform_remote_state.base.outputs.region
+  sns_arn             = data.terraform_remote_state.base.outputs.sns_arn
+  alb_name            = "app/denilson-alb/791f97d87da8bd47"
+  api_name            = "denilson-prd-sells-service-prd-sam-app"
+  log_group_name      = "/aws/ecs/worker"
+  statistic           = "Sum"
+  comparison_operator = "GreaterThanThreshold"
+  treat_missing_data  = "notBreaching"
+  evaluation_periods  = "2"
+  period              = "60"
+  threshold           = "1"
+  stat                = "Sum"
+  unit                = "Count"
+
+  type        = "metric"
+  y           = 0
+  width       = 8
+  height      = 6
+  view        = "timeSeries"
+  period_dash = 60
+  stacked     = true
+
+  actions = [
+    local.sns_arn
+  ]
 }
--- iaas/applications/monitoring/main.tf
+++ iaas/applications/monitoring/main.tf
@@ -5,92 +5,86 @@ resource "aws_cloudwatch_dashboard" "main" {
     {
       "widgets" : [
         {
-          "type" : "metric",
-          "x" : 8,
-          "y" : 0,
-          "width" : 8,
-          "height" : 6,
+          "type" : local.type,
+          "x" : 0,
+          "y" : local.y,
+          "width" : local.width,
+          "height" : local.height,
           "properties" : {
+            "title" : "GraphQL API ALB",
+            "view" : local.view,
+            "stacked" : local.stacked,
+            "region" : local.region,
+            "stat" : local.stat,
+            "period" : local.period_dash,
             "metrics" : [
               [
                 "AWS/ApplicationELB",
                 "ActiveConnectionCount",
                 "LoadBalancer",
-                "app/denilson-alb/791f97d87da8bd47",
+                local.alb_name,
                 { "color" : "#2ca02c" }
               ],
               [".", "RequestCount", ".", ".", { "color" : "#bcbd22" }],
               [".", "HTTPCode_ELB_4XX_Count", ".", ".", { "color" : "#ff7f0e" }],
               [".", "HTTPCode_Target_5XX_Count", ".", ".", { "color" : "#d62728" }],
             ],
-            "view" : "timeSeries",
-            "stacked" : true,
-            "region" : "us-east-1",
-            "stat" : "Sum",
-            "period" : 60
-            "title" : "GraphQL API ALB"
           }
         },
         {
-          "type" : "metric",
-          "x" : 0,
-          "y" : 0,
-          "width" : 8,
-          "height" : 6,
+          "type" : local.type,
+          "x" : 8,
+          "y" : local.y,
+          "width" : local.width,
+          "height" : local.height,
           "properties" : {
+            "title" : "Sells-Service API Gateway",
+            "view" : local.view,
+            "stacked" : local.stacked,
+            "region" : local.region,
+            "stat" : local.stat,
+            "period" : local.period_dash,
             "metrics" : [
               [
                 "AWS/ApiGateway",
                 "4XXError",
                 "ApiName",
-                "denilson-prd-sells-service-prd-sam-app", {
+                local.api_name, {
                   "id" : "m1", "yAxis" : "left", "color" : "#ff7f0e"
                 }
               ],
               [".", "5XXError", ".", ".", { "color" : "#d62728" }],
               [".", "Count", ".", ".", { "color" : "#2ca02c" }]
             ],
-            "view" : "timeSeries",
-            "stacked" : true,
-            "region" : "us-east-1",
-            "stat" : "Sum",
-            "period" : 60
-            "title" : "Sells-Service API Gateway"
           }
         },
         {
-          "type" : "metric",
+          "type" : local.type,
           "x" : 16,
-          "y" : 0,
-          "width" : 8,
-          "height" : 6,
+          "y" : local.y,
+          "width" : local.width,
+          "height" : local.height,
           "properties" : {
+            "title" : "Worker Incoming Logs",
+            "view" : local.view,
+            "stacked" : local.stacked,
+            "region" : local.region,
+            "stat" : local.stat,
+            "period" : local.period_dash,
             "metrics" : [
+              [
+                "AWS/Logs",
+                "IncomingLogEvents",
+                "LogGroupName",
+                local.log_group_name, {
+                  "id" : "m1", "yAxis" : "left", "color" : "#2ca02c"
+                }
+              ],
               ["Custom", "ErrorCount", { "color" : "#d62728" }]
             ],
-            "view" : "timeSeries",
-            "stacked" : true,
-            "region" : "us-east-1",
-            "stat" : "Sum",
-            "period" : 60,
-            "title" : "Worker Incoming Logs"
           }
         }
       ]
     }
   )
 }
-
-resource "aws_cloudwatch_log_metric_filter" "worker_error" {
-  name           = "WorkerErrors"
-  pattern        = "Error"
-  log_group_name = "/aws/ecs/worker"
-
-  metric_transformation {
-    name          = "ErrorCount"
-    namespace     = "Custom"
-    value         = "1"
-    default_value = "0"
-  }
-}
-
